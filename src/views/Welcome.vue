81b16974d6cec81c5f8a969bf9804bf14d5553f8
Outer Cloud Studio <outercloudstudio@gmail.com>
2022-12-28 20:21:16 -0600

    Welcome Animation

--- src/views/Welcome.vue
+++ src/views/Welcome.vue
@@ -1,12 +1,186 @@
 <template>
-  <h1>Vector Engine</h1>
+  <div id="component">
+    <canvas ref="canvas"></canvas>
 
-  <button @click="router.push({ name: 'Projects' })">Go!</button>
+    <div id="wrapper">
+      <h1>Vector Engine</h1>
+
+      <button @click="router.push({ name: 'Projects' })">Let's Go!</button>
+    </div>
+  </div>
 </template>
 
 <script setup lang="ts">
-import { ref, computed } from 'vue'
+import { ref, onMounted, Ref } from 'vue'
 import router from '@/router'
+
+const canvas: Ref<null | HTMLCanvasElement> = ref(null)
+
+const shapeSize = 200
+const shapeSpeed = 5
+const fadeIn = 1
+const rotationSpeed = 1
+const lineWidth = 10
+
+function randomInteger(min: number, max: number) {
+  return Math.floor(Math.random() * (max - min) + min)
+}
+
+function randomShape() {
+  if (!canvas.value)
+    return {
+      points: randomInteger(3, 7),
+      x: randomInteger(-100, 100),
+      y: randomInteger(-100, 100),
+      distance: 60,
+      opacity: 0,
+      rotation: Math.random() * Math.PI * 2,
+      rotationSpeed: Math.random() * 2 - 1,
+    }
+
+  return {
+    points: randomInteger(3, 7),
+    x: (randomInteger(-100, 100) * (canvas.value.width | 0)) / 500,
+    y: (randomInteger(-100, 100) * (canvas.value.height | 0)) / 500,
+    distance: 60,
+    opacity: 0,
+    rotation: Math.random() * Math.PI * 2,
+    rotationSpeed: Math.random() * 2 - 1,
+  }
+}
+
+onMounted(() => {
+  const context = canvas.value?.getContext('2d')
+
+  if (!canvas.value) return
+
+  canvas.value.width = canvas.value?.offsetWidth
+  canvas.value.height = canvas.value?.offsetHeight
+
+  addEventListener('resize', () => {
+    if (!canvas.value) return
+
+    canvas.value.width = canvas.value?.offsetWidth
+    canvas.value.height = canvas.value?.offsetHeight
+  })
+
+  let shapes: {
+    points: number
+    x: number
+    y: number
+    distance: number
+    opacity: number
+    rotation: number
+    rotationSpeed: number
+  }[] = []
+
+  for (let i = 0; i < 100; i++) {
+    shapes.push(randomShape())
+
+    shapes[i].distance = randomInteger(0, 60)
+    shapes[i].opacity = 1
+  }
+
+  let lastTime = -1
+
+  function drawFrame(timestamp: DOMHighResTimeStamp) {
+    if (!canvas.value) return
+    if (!context) return
+
+    const deltaTime = (timestamp - lastTime) / 1000
+
+    lastTime = timestamp
+
+    if (lastTime == -1 || deltaTime > 0.1) {
+      window.requestAnimationFrame(drawFrame)
+
+      return
+    }
+
+    const centerX = canvas.value.width / 2
+    const centerY = canvas.value.height / 2
+
+    context.clearRect(0, 0, canvas.value.width, canvas.value.height)
+
+    for (let shapeIndex = 0; shapeIndex < shapes.length; shapeIndex++) {
+      let shape = shapes[shapeIndex]
+
+      shape.distance -= shapeSpeed * deltaTime
+      shape.opacity += fadeIn * deltaTime
+      shape.rotation += shape.rotationSpeed * rotationSpeed * deltaTime
+
+      if (shape.distance < 0) {
+        shapes[shapeIndex] = randomShape()
+        shape = shapes[shapeIndex]
+      }
+
+      const renderSize = shapeSize / shape.distance
+
+      const renderX = centerX + (shape.x / shape.distance) * 90
+      const renderY = centerY + (shape.y / shape.distance) * 90
+
+      const sideAngle = (Math.PI * 2) / shape.points
+
+      context.strokeStyle = `rgba(255, 255, 255, ${shape.opacity})`
+      context.lineWidth = lineWidth * (renderSize / shapeSize)
+      context.lineCap = 'round'
+
+      context.beginPath()
+
+      context.moveTo(Math.cos(shape.rotation) * renderSize + renderX, Math.sin(shape.rotation) * renderSize + renderY)
+
+      for (let side = 1; side <= shape.points; side++) {
+        context.lineTo(Math.cos(side * sideAngle + shape.rotation) * renderSize + renderX, Math.sin(side * sideAngle + shape.rotation) * renderSize + renderY)
+      }
+
+      context.stroke()
+    }
+
+    window.requestAnimationFrame(drawFrame)
+  }
+
+  window.requestAnimationFrame(drawFrame)
+})
 </script>
 
-<style scoped></style>
+<style scoped>
+#component {
+  background-color: var(--main);
+}
+
+#wrapper {
+  width: 100vw;
+  height: 100vh;
+
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+
+  position: relative;
+}
+
+canvas {
+  position: absolute;
+
+  width: 100vw;
+  height: 100vh;
+}
+
+h1 {
+  display: inline-block;
+
+  font-weight: 700;
+  font-size: 36px;
+
+  margin-bottom: 4rem;
+}
+
+button {
+  background-color: var(--grab);
+  border-radius: 5px;
+  box-shadow: 0 0 1.5rem 0.1rem var(--grab);
+
+  padding: 0.25rem 0.5rem;
+}
+</style>
