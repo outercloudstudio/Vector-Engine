3f94fae072dd47dea610d85273d2c5beb3b0b27b
Outer Cloud Studio <outercloudstudio@gmail.com>
2023-07-05 01:32:02 -0500

    feat: tauri watching files

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,10 +1,12 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-use std::path::Path;
+use std::{
+	path::Path,
+	sync::{Arc, Mutex},
+};
 
-use notify::{RecommendedWatcher, RecursiveMode, Result, Watcher};
-use tauri::Manager;
+use notify::{RecursiveMode, Watcher};
 
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 #[tauri::command]
@@ -13,17 +15,38 @@ fn greet(name: &str) -> String {
 }
 
 fn main() {
+	let watcher = notify::recommended_watcher(|res| match res {
+		Ok(event) => println!("event: {:?}", event),
+		Err(e) => println!("watch error: {:?}", e),
+	})
+	.unwrap();
+
+	let watcher_arc_mutex = Arc::new(Mutex::new(watcher));
+
+	let watcher_arc_mutex_setup_reference = watcher_arc_mutex.clone();
+
 	tauri::Builder::default()
 		.invoke_handler(tauri::generate_handler![greet])
-		.setup(|app| {
+		.setup(move |_app| {
 			println!("Setup app!");
 
-			let mut watcher = notify::recommended_watcher(|res| match res {
-				Ok(event) => println!("event: {:?}", event),
-				Err(e) => println!("watch error: {:?}", e),
-			})?;
+			let mut watcher_lock = watcher_arc_mutex_setup_reference.lock().unwrap();
 
-			watcher.watch(Path::new("."), RecursiveMode::Recursive)?;
+			watcher_lock
+				.watch(
+					Path::new("D:/Vector Engine Projects/Tauri/"),
+					RecursiveMode::Recursive,
+				)
+				.unwrap();
+
+			println!(
+				"{}",
+				Path::new("D:/Vector Engine Projects/Tauri/")
+					.to_str()
+					.unwrap()
+			);
+
+			println!("Finished Setup!");
 
 			// let test_code = "alert('Worked!')";
 
