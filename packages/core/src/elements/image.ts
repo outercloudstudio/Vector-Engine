52be19dcc34f69e569ed404d078eed2be060ae40
Outer Cloud Studio <outercloudstudio@gmail.com>
2023-08-03 20:06:19 -0500

    chore: remove all of shader test

--- packages/core/src/elements/image.ts
+++ packages/core/src/elements/image.ts
@@ -1,47 +1,6 @@
 import { ImageAsset } from '../assets/image'
 import { Vector2 } from '../vector'
-import { createProgramFromScripts } from '../webgl'
 import { Element } from './element'
-
-const vertexShaderSource = `
-attribute vec2 a_position;
-attribute vec2 a_texCoord;
-
-uniform vec2 u_resolution;
-
-varying vec2 v_texCoord;
-
-void main() {
-  // convert the rectangle from pixels to 0.0 to 1.0
-  vec2 zeroToOne = a_position / u_resolution;
-
-  // convert from 0->1 to 0->2
-  vec2 zeroToTwo = zeroToOne * 2.0;
-
-  // convert from 0->2 to -1->+1 (clipspace)
-  vec2 clipSpace = zeroToTwo - 1.0;
-
-  gl_Position = vec4(clipSpace * vec2(1, -1), 0, 1);
-
-  // pass the texCoord to the fragment shader
-  // The GPU will interpolate this value between points.
-  v_texCoord = a_texCoord;
-}
-`
-
-const fragmentShaderSource = `
-precision mediump float;
-
-// our texture
-uniform sampler2D u_image;
-
-// the texCoords passed in from the vertex shader.
-varying vec2 v_texCoord;
-
-void main() {
-  gl_FragColor = texture2D(u_image, v_texCoord).bgra;
-}
-`
 
 export class VectorImage extends Element {
 	public image: ImageAsset
