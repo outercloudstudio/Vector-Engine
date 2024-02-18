a88301abf3b80c6730eceeb1c47501d5bfde48ba
Outer Cloud Studio <outercloudstudio@gmail.com>
2024-02-18 16:42:35 -0600

    Resizing

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -49,12 +49,22 @@ fn main() -> Result<()> {
     // App
     let mut app = unsafe { App::create(&window)? };
     let mut destroying = false;
+    let mut minimized = false;
     event_loop.run(move |event, _, control_flow| {
         *control_flow = ControlFlow::Poll;
         match event {
             // Render a frame if our Vulkan app is not being destroyed.
-            Event::MainEventsCleared if !destroying => unsafe { app.render(&window) }.unwrap(),
-            // Destroy our Vulkan app.
+            Event::MainEventsCleared if !destroying && !minimized => unsafe { app.render(&window) }.unwrap(),
+            Event::WindowEvent {
+                event: WindowEvent::Resized(size), ..
+            } => {
+                if size.width == 0 || size.height == 0 {
+                    minimized = true;
+                } else {
+                    minimized = false;
+                    app.resized = true;
+                }
+            }
             Event::WindowEvent {
                 event: WindowEvent::CloseRequested, ..
             } => {
@@ -77,6 +87,7 @@ struct App {
     data: AppData,
     device: Device,
     frame: usize,
+    resized: bool,
 }
 
 const MAX_FRAMES_IN_FLIGHT: usize = 2;
@@ -110,6 +121,7 @@ impl App {
             data,
             device,
             frame: 0,
+            resized: false,
         })
     }
 
@@ -117,10 +129,15 @@ impl App {
     unsafe fn render(&mut self, window: &Window) -> Result<()> {
         self.device.wait_for_fences(&[self.data.in_flight_fences[self.frame]], true, u64::MAX)?;
 
-        let image_index = self
+        let result = self
             .device
-            .acquire_next_image_khr(self.data.swapchain, u64::MAX, self.data.image_available_semaphores[self.frame], vk::Fence::null())?
-            .0 as usize;
+            .acquire_next_image_khr(self.data.swapchain, u64::MAX, self.data.image_available_semaphores[self.frame], vk::Fence::null());
+
+        let image_index = match result {
+            Ok((image_index, _)) => image_index as usize,
+            Err(vk::ErrorCode::OUT_OF_DATE_KHR) => return self.recreate_swapchain(window),
+            Err(e) => return Err(anyhow!(e)),
+        };
 
         if !self.data.images_in_flight[image_index as usize].is_null() {
             self.device.wait_for_fences(&[self.data.images_in_flight[image_index as usize]], true, u64::MAX)?;
@@ -146,7 +163,16 @@ impl App {
         let image_indices = &[image_index as u32];
         let present_info = vk::PresentInfoKHR::builder().wait_semaphores(signal_semaphores).swapchains(swapchains).image_indices(image_indices);
 
-        self.device.queue_present_khr(self.data.present_queue, &present_info)?;
+        let result = self.device.queue_present_khr(self.data.present_queue, &present_info);
+
+        let changed = result == Ok(vk::SuccessCode::SUBOPTIMAL_KHR) || result == Err(vk::ErrorCode::OUT_OF_DATE_KHR);
+
+        if self.resized || changed {
+            self.resized = false;
+            self.recreate_swapchain(window)?;
+        } else if let Err(e) = result {
+            return Err(anyhow!(e));
+        }
 
         self.frame = (self.frame + 1) % MAX_FRAMES_IN_FLIGHT;
 
@@ -157,33 +183,43 @@ impl App {
     unsafe fn destroy(&mut self) {
         self.device.device_wait_idle().unwrap();
 
-        self.data.in_flight_fences.iter().for_each(|f| self.device.destroy_fence(*f, None));
+        self.destroy_swapchain();
 
+        self.data.in_flight_fences.iter().for_each(|f| self.device.destroy_fence(*f, None));
         self.data.render_finished_semaphores.iter().for_each(|s| self.device.destroy_semaphore(*s, None));
         self.data.image_available_semaphores.iter().for_each(|s| self.device.destroy_semaphore(*s, None));
-
         self.device.destroy_command_pool(self.data.command_pool, None);
-
-        self.data.framebuffers.iter().for_each(|f| self.device.destroy_framebuffer(*f, None));
-
-        self.device.destroy_pipeline(self.data.pipeline, None);
-
-        self.device.destroy_pipeline_layout(self.data.pipeline_layout, None);
-        self.device.destroy_render_pass(self.data.render_pass, None);
-
-        self.data.swapchain_image_views.iter().for_each(|v| self.device.destroy_image_view(*v, None));
-
-        self.device.destroy_swapchain_khr(self.data.swapchain, None);
+        self.device.destroy_device(None);
+        self.instance.destroy_surface_khr(self.data.surface, None);
 
         if VALIDATION_ENABLED {
             self.instance.destroy_debug_utils_messenger_ext(self.data.messenger, None);
         }
 
-        self.device.destroy_device(None);
-
-        self.instance.destroy_surface_khr(self.data.surface, None);
-
         self.instance.destroy_instance(None);
+    }
+
+    unsafe fn destroy_swapchain(&mut self) {
+        self.data.framebuffers.iter().for_each(|f| self.device.destroy_framebuffer(*f, None));
+        self.device.free_command_buffers(self.data.command_pool, &self.data.command_buffers);
+        self.device.destroy_pipeline(self.data.pipeline, None);
+        self.device.destroy_pipeline_layout(self.data.pipeline_layout, None);
+        self.device.destroy_render_pass(self.data.render_pass, None);
+        self.data.swapchain_image_views.iter().for_each(|v| self.device.destroy_image_view(*v, None));
+        self.device.destroy_swapchain_khr(self.data.swapchain, None);
+    }
+
+    unsafe fn recreate_swapchain(&mut self, window: &Window) -> Result<()> {
+        self.device.device_wait_idle()?;
+        self.destroy_swapchain();
+        create_swapchain(window, &self.instance, &self.device, &mut self.data)?;
+        create_swapchain_image_views(&self.device, &mut self.data)?;
+        create_render_pass(&self.instance, &self.device, &mut self.data)?;
+        create_pipeline(&self.device, &mut self.data)?;
+        create_framebuffers(&self.device, &mut self.data)?;
+        create_command_buffers(&self.device, &mut self.data)?;
+        self.data.images_in_flight.resize(self.data.swapchain_images.len(), vk::Fence::null());
+        Ok(())
     }
 }
 
