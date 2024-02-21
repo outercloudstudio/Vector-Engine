409e0c7899a1f6308b19a1e2b5f0b0f368d6a526
Outer Cloud Studio <outercloudstudio@gmail.com>
2024-02-20 23:02:03 -0600

    more advanced js

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,36 +1,54 @@
-// // Prevents additional console window on Windows in release, DO NOT REMOVE!!
-// #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+use deno_core::error::AnyError;
+use deno_core::{op, Extension};
+use std::env;
+use std::path::Path;
+use std::rc::Rc;
 
-// // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-// #[tauri::command]
-// fn greet(name: &str) -> String {
-//     format!("Hello, {}! You've been greeted from Rust!", name)
-// }
+deno_core::extension!(runtime_extension, ops = [op_read_file, op_write_file, op_remove_file]);
 
-// fn main() {
-//     tauri::Builder::default()
-//         .invoke_handler(tauri::generate_handler![greet])
-//         .run(tauri::generate_context!())
-//         .expect("error while running tauri application");
-// }
+async fn run_js(file_path: &str) -> Result<(), AnyError> {
+    let main_module = deno_core::resolve_path(file_path, Path::new(env::current_dir().unwrap().as_path()))?;
 
-use deno_core::{JsRuntime, RuntimeOptions};
+    let mut js_runtime = deno_core::JsRuntime::new(deno_core::RuntimeOptions {
+        module_loader: Some(Rc::new(deno_core::FsModuleLoader)),
+        extensions: vec![runtime_extension::init_ops()],
+        ..Default::default()
+    });
 
-fn main() {
-    let mut runtime = JsRuntime::new(RuntimeOptions { ..Default::default() });
+    js_runtime
+        .execute_script("[runjs:runtime.js]", deno_core::FastString::from_static(include_str!("./runtime.js")))
+        .unwrap();
 
-    runtime
-        .execute_script_static(
-            "<usage>",
-            r#"
-// Print helper function, calling Deno.core.print()
-function print(value) {
-  Deno.core.print(value.toString()+"\n");
+    let mod_id = js_runtime.load_main_module(&main_module, None).await?;
+    let result = js_runtime.mod_evaluate(mod_id);
+    js_runtime.run_event_loop(false).await?;
+    result.await?
 }
 
-const arr = [1, 2, 3];
-print(arr);
-"#,
-        )
-        .unwrap();
+fn main() {
+    println!("Path: {}", env::current_dir().unwrap().to_str().unwrap());
+
+    let runtime = tokio::runtime::Builder::new_current_thread().enable_all().build().unwrap();
+
+    if let Err(error) = runtime.block_on(run_js("./example.js")) {
+        eprintln!("error: {}", error);
+    }
+}
+
+#[op]
+async fn op_read_file(path: String) -> Result<String, AnyError> {
+    let contents = tokio::fs::read_to_string(path).await?;
+    Ok(contents)
+}
+
+#[op]
+async fn op_write_file(path: String, contents: String) -> Result<(), AnyError> {
+    tokio::fs::write(path, contents).await?;
+    Ok(())
+}
+
+#[op]
+fn op_remove_file(path: String) -> Result<(), AnyError> {
+    std::fs::remove_file(path)?;
+    Ok(())
 }
