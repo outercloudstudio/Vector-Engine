bbddcf50b77731cd6bbc618be8615b61db5ce24d
Outer Cloud Studio <outercloudstudio@gmail.com>
2024-02-24 20:02:25 -0600

    Working window

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,15 +1,63 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-// Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
+mod renderer;
+mod runtime;
+
+use log::info;
+use std::{env, fs::File, io::BufWriter, sync::Mutex};
+use tauri::Manager;
+
+use renderer::Renderer;
+use runtime::Runtime;
+
+struct Timeline {}
+
+struct Clip {}
+
+struct Project {
+    timeline: Timeline,
+    clips: Vec<Clip>,
+    renderer: Renderer,
+    runtime: Runtime,
+}
+
 #[tauri::command]
 fn greet(name: &str) -> String {
     format!("Hello, {}! You've been greeted from Rust!", name)
 }
 
 fn main() {
+    let timeline = Timeline {};
+    let clips = vec![];
+    let renderer = Renderer::create();
+    let runtime = Runtime {};
+
+    let project = Project { timeline, clips, renderer, runtime };
+
     tauri::Builder::default()
         .invoke_handler(tauri::generate_handler![greet])
+        .setup(|app| {
+            #[cfg(debug_assertions)] // only include this code on debug builds
+            {
+                let window = app.handle().get_window("main").unwrap();
+
+                window.open_devtools();
+                window.close_devtools();
+            }
+
+            Ok(())
+        })
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
+
+// app.run(|app_handle, event| match event {
+//     tauri::RunEvent::ExitRequested { .. } => {
+//         let project_mutex = app_handle.state::<Mutex<Project>>();
+//         let mut project = project_mutex.lock().unwrap();
+
+//         destroy_renderer(&mut project.renderer).unwrap();
+//     }
+//     _ => {}
+// });
