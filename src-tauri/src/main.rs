cfd5104c092a4f5bf7122b2465e8ff4fccf6301a
Outer Cloud Studio <outercloudstudio@gmail.com>
2024-02-24 20:14:17 -0600

    Render to preview!

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -6,7 +6,7 @@ mod runtime;
 
 use log::info;
 use std::{env, fs::File, io::BufWriter, sync::Mutex};
-use tauri::Manager;
+use tauri::{Manager, State};
 
 use renderer::Renderer;
 use runtime::Runtime;
@@ -23,8 +23,21 @@ struct Project {
 }
 
 #[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
+fn preview(project_mutex: State<Mutex<Project>>) -> Vec<u8> {
+    println!("Rendering preview...");
+
+    let mut project = project_mutex.lock().unwrap();
+    let bytes = project.renderer.render();
+
+    let mut encoded_bytes: Vec<u8> = vec![];
+    let mut encoder = png::Encoder::new(&mut encoded_bytes, 512, 512);
+    encoder.set_color(png::ColorType::Rgba);
+    encoder.set_depth(png::BitDepth::Eight);
+    let mut writer = encoder.write_header().unwrap();
+    writer.write_image_data(&bytes).unwrap();
+    writer.finish().unwrap();
+
+    return encoded_bytes;
 }
 
 fn main() {
@@ -34,9 +47,11 @@ fn main() {
     let runtime = Runtime {};
 
     let project = Project { timeline, clips, renderer, runtime };
+    let project_mutex = Mutex::new(project);
 
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![greet])
+        .manage(project_mutex)
+        .invoke_handler(tauri::generate_handler![preview])
         .setup(|app| {
             #[cfg(debug_assertions)] // only include this code on debug builds
             {
